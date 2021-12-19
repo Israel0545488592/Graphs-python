03062c4e9eef3afb6a94c8f21464d76f605516cf
Israel Jacobovitz <learsi0545488592@gmail.com>
2021-12-19 07:14:39 +0200

    figured out a a simple efficient data structure for various purposes sort of like a dynamic hashMap( python is great)
    :)

--- Classes.py
+++ Classes.py
@@ -1,3 +1,25 @@
+class TagMap:
+    def __init__(self):
+        self.values = []
+        self.freeMemory = [0]
+
+    def get(self, tag):
+        return self.values[tag]
+
+    def add(self, val):
+        val.tag = self.freeMemmory[0]
+        self.freeMemmory.pop(0)
+        if len(self.freeMemmory) == 0:
+            self.freeMemmory.insert(0, len(self.values))
+            self.nodes.append(val)
+        else:
+            self.values.insert(val.tag, val)
+
+    def remove(self, tag):
+        t = self.values[tag]
+        self.values[tag] = None
+        self.freeMemmory.insert(0, t)
+
 class Geolocation:
     def __init__(self, x, y, z):
         self.x = x; self.y = y, self.z = z;
@@ -14,6 +36,7 @@ class Edge:
         self.src = src
         self.dst = dst
         self.weight = w
+        self.dag = 0
 
 class Path:
     def __init__(self):
@@ -43,7 +66,9 @@ class Vertecies:
         self.size +=1
 
     def removeNode(self, id):
-        self.freeMemmory.insert(0, self.nodes.pop(id).key)
+        t = self.values[id]
+        self.values[id] = None
+        self.freeMemmory.insert(0, t)
         self.size -=1
 
 class Edges:
@@ -52,9 +77,11 @@ class Edges:
         self.size = 0
 
     def addEdge(self, edge):
-        self.edges[edge.src].insert(edge) #we need to figure out a data
-                                          #structure for it
+        self.edges[edge.src].add(edge)
         self.size +=1
+
+    def rmeove(self, edge):
+        self.edges[edge.src].remove(edge.dst)
 
 class Graph:
     def __init__(self):
